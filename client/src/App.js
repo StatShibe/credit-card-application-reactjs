b4174d3a1643f1932d1ee7aa8b823a5256d3123a
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2023-11-17 13:39:37 +0530

    editing LoginPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
 import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, this app works</h1>
       <LoginPage/>
     </div>
   );
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,14 +1,33 @@
 import React from "react";
+import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from "react-bootstrap/esm/FloatingLabel";
+
+const styling = {
+    backgroundColor : 'blue',
+    textAlign : 'center',
+    width : '200px',
+    marginLeft : '50%',
+    borderRadius : '15px',
+    marginTop : '200px'
+};
 
 function LoginPage(){
-    return (
-        <div>
-            <p>I am Haresh M</p>
-            <Card>
-                <Card.Body>This is some text within a card body.</Card.Body>
-            </Card>
-        </div>
+    return(
+        <Container>
+            <Form>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Example textarea</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+    </Form>
+        </Container>
     );
 }
 
