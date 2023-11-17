581221d9af6b45289e34359e990120a23d13e094
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2023-11-17 09:58:35 +0530

    Changes

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+
+function LoginPage(){
+    return (
+        <div>
+            <p>I am Haresh M</p>
+            <Card>
+                <Card.Body>This is some text within a card body.</Card.Body>
+            </Card>
+        </div>
+    );
+}
+
+export default LoginPage;
