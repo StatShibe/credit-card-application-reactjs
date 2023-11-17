571a22d91ef3b0ac7584a91ae416d9cf7797ae17
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2023-11-17 16:32:43 +0530

    added home page

---
+++ client/src/components/DisplayCarousel.jsx
@@ -0,0 +1,50 @@
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import {Carousel } from 'react-bootstrap';  
+import img1 from '../images/carousel-img1.jpg';  
+import img2 from '../images/carousel-img2.jpg';
+import img3 from '../images/carousel-img3.jpg';
+function DisplayCarousel() {  
+  
+  return (   
+    <div>  
+    <Carousel>  
+  <Carousel.Item>  
+  <img style={{maxHeight:"500px"}}  
+      className="d-block w-100"  
+      src={img1}  
+      alt="First slide"  
+    />  
+    <Carousel.Caption>  
+      <h3>Applying for a Credit Card is just a click away!</h3>  
+      <p>Grab your own credit card by applying online</p>  
+    </Carousel.Caption>  
+  </Carousel.Item>  
+  <Carousel.Item>  
+    <img style={{maxHeight:"500px"}} 
+      className="d-block w-100"  
+      src={img2}  
+      alt="Second slide"  
+    />  
+  
+    <Carousel.Caption>  
+      <h3>Special benefits for using Credit card during festival time!</h3>  
+      <p>Cashback and bonus benefits while shopping with selected vendors</p>  
+    </Carousel.Caption>  
+  </Carousel.Item>  
+  <Carousel.Item>  
+    <img  style={{maxHeight:"500px"}}
+      className="d-block w-100"  
+      src={img3}  
+      alt="Third slide"  
+    />
+  
+    <Carousel.Caption>  
+      <h3>Enjoy exclusive credit score scaling interests!</h3>  
+      <p>You got a good credit score, then we got you!!</p>  
+    </Carousel.Caption>  
+  </Carousel.Item>  
+</Carousel>  
+</div>  
+  );  
+}  
+export default DisplayCarousel;  
--- client/src/pages/CustomerDashboard.jsx
+++ client/src/pages/CustomerDashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Container from 'react-bootstrap/Container'
 
-import Button from "react-bootstrap/Button";
+import DisplayCarousel from "../components/DisplayCarousel";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Nav from 'react-bootstrap/Nav';
@@ -9,18 +9,21 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function NavigationBar() {
   return (
+    <>
     <div>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="/Dashboard">Home</Nav.Link>
+            <Nav.Link href="#features">Your Applications</Nav.Link>
+            <Nav.Link href="#pricing">Info</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
     </div>
+    <DisplayCarousel/>
+    </>
   );
 }
 
